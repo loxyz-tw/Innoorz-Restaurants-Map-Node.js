7bda50842a83dd7bcfcdbc6b483266bdab3f6fd4
LoRoy <loxyz.tw@gmail.com>
2016-04-06 17:35:20 +0800

    add star column, get restaurants order by star desc

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function(request, response) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM inno_restaurant', function(err, result) {
+    client.query('SELECT * FROM inno_restaurant ORDER BY star DESC', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
